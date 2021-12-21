f30c5dfb23a8dac3b4bcec8ffe10d40a285a593f
Hejx <531601727@qq.com>
2021-12-21 22:48:21 +0800

    feat(the first commit): the first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const version = require('./package.json').version;
+const cc = require('conventional-changelog');
+
+const file = `./RELEASE_NOTE_${version}.md`;
+const fileStream = require('fs').createWriteStream(file);
+
+cc({
+  preset: 'angular',
+  pkg: {
+    transform (pkg) {
+      pkg.version = `v${version}`;
+      return pkg;
+    },
+  },
+  releaseCount: 0,
+}).pipe(fileStream).on('close', () => {
+  console.log(`Generated release note at ${file}`);
+});
